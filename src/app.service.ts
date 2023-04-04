0382f887e28b0c4a9fc617be91d403c6deab6cd9
mdfaisalahmed025 <71064436+mdfaisalahmed025@users.noreply.github.com>
2023-04-04 11:44:18 +0600

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'welcome to flyfar ladies test!';
+    return 'welcome to flyfar ladies testwwww!';
   }
 }
